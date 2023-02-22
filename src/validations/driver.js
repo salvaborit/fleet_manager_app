1e09784c49dc00d4450d998bc352c8e813fe4a0c
Salvador Borit <salvaborit@gmail.com>
2023-02-22 00:20:44 -0300

    corrected validations

--- src/validations/driver.js
+++ src/validations/driver.js
@@ -18,11 +18,7 @@ export const newDriverSchema = yup.object().shape({
     .email("Must be an email address")
     .max(127, "Less than 127 characters")
     .required("Required"),
-  phone: yup
-    .number()
-    .max(31, "Less than 31 characters")
-    .typeError("Must be a number")
-    .required("Required"),
+  phone: yup.number().typeError("Must be a number").required("Required"),
   birthdate: yup.date().required("Required"),
   id_type: yup
     .string()
@@ -38,8 +34,11 @@ export const driverFilterSchema = yup.object().shape({
   first_name: yup.string().max(63, "Less than 63 characters"),
   last_name: yup.string().max(63, "Less than 63 characters"),
   address: yup.string().max(255, "Less than 255 characters"),
-  email: yup.string().max(127, "Less than 127 characters"),
-  phone: yup.string().max(31, "Less than 31 characters"),
+  email: yup
+    .string()
+    .email("Must be an email address")
+    .max(127, "Less than 127 characters"),
+  phone: yup.number().typeError("Must be a number"),
   birthdate: yup.date(),
   id_type: yup.string(),
   id_number: yup
