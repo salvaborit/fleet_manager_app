86e4a1b50aec73891aa0e927818f1cdeae76f695
Salvador Borit <salvaborit@gmail.com>
2023-02-22 00:38:41 -0300

    more space between loading sign and screen bottom

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import { CgSpinner } from "react-icons/cg";
 function Loading() {
   return (
     <div
-      className="z-50 absolute bottom-2 right-1/2 h-12 w-32 bg-neutral-500/50 rounded-xl
+      className="z-50 absolute bottom-4 right-1/2 h-12 w-32 bg-neutral-500/50 rounded-xl
       flex justify-center items-center"
     >
       <CgSpinner size={40} className="text-neutral-50 animate-spin" />
