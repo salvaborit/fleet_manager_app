ed87ed84219b28741ed0eeee4b0c0edb014ca283
Salvador Borit <salvaborit@gmail.com>
2023-02-25 11:39:17 -0300

    modals working

--- src/components/VehiclesRoute.jsx
+++ src/components/VehiclesRoute.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { TbTruck } from "react-icons/tb";
 import { useRoutes } from "react-router-dom";
 import Loading from "./Loading";
-import VehicleListView from "./VehicleListView";
 import VehiclesList from "./VehiclesList";
 import VehiclesToolbar from "./VehiclesToolbar";
 
@@ -11,7 +10,10 @@ function VehiclesRoute({ isOpenNew }) {
   const [vehiclesList, setVehiclesList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const routes = useRoutes([{ path: "/", element: <VehicleListView /> }]);
+  // const routes = useRoutes([
+  //   {path: '/',
+  // element: < />}
+  // ])
 
   async function fetchData() {
     setIsLoading(true);
@@ -41,7 +43,24 @@ function VehiclesRoute({ isOpenNew }) {
 
   return (
     <>
-      <div className="w-full h-full flex flex-col">{routes}</div>
+      <div className="w-full h-full flex flex-col">
+        <div className="flex items-center mr-16 mb-8 mt-6">
+          <TbTruck size={40} className="text-neutral-500" />
+          <h1 className="text-2xl font-bold text-neutral-500 ml-8">Vehicles</h1>
+        </div>
+
+        <div className="flex flex-col bg-neutral-50 rounded-2xl shadow-lg">
+          <div>
+            <VehiclesToolbar
+              fetchFilteredData={fetchFilteredData}
+              isLoading={isLoading}
+            />
+          </div>
+          <div className="container self-center pb-8">
+            {!isLoading && <VehiclesList vehiclesList={vehiclesList} />}
+          </div>
+        </div>
+      </div>
       {isLoading && <Loading />}
     </>
   );
